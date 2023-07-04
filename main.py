170d8c6a1d23f1d59432c947fd059f90d610ca0d
Raushan Kumar <116261190+Raushan077@users.noreply.github.com>
2023-07-04 22:32:23 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,20 @@
+import random
+
+number = random.randint(1,100)
+guess = 0
+
+while guess !=number:
+
+    guess = int(input("Enter Guess: "))
+
+    if (guess < number):
+        print("Guess higher!")
+
+    elif(guess > number):
+        print("Guess lower!")
+
+    else:
+        print("You Won!")
+
+
+        # Raushan Kumar
